1ab261ff2db363206103199e3e5dc1fe9b2d5759
Arash Mousavi <mousavi.arash@gmail.com>
2014-05-13 22:05:12 +0430

    adds Po class

---
+++ lib/poparser/po.rb
@@ -0,0 +1,100 @@
+module PoParser
+  # Po class keeps all entries of a Po file
+  # 
+  class Po
+    include Enumerable
+
+    def initialize
+      @entries = []
+    end
+
+    # add new entries to po file
+    # 
+    # @example
+    #   entry = { 
+    #             translator_comment: 'comment',
+    #             refrence: 'refrense comment',
+    #             flag: 'fuzzy',
+    #             msgstr: 'translatable string',
+    #             msgstr: 'translation'
+    #           }
+    #   add_entry(entry)
+    # 
+    # @param entry [Hash, Array] a hash of entry contents or an array of hashes
+    def add_entry(entry)
+      if entry.kind_of? Hash
+        @entries << Entry.new(entry)
+        @entries.last
+      elsif entry.kind_of? Array
+        entry.each do |en|
+          @entries << Entry.new(en)
+        end
+      else
+        raise ArgumentError, 'Must be a hash or an array of hashes'
+      end
+    end
+    alias_method :<<, :add_entry
+
+    # Finds all entries that are flaged as fuzzy
+    # 
+    # @return [Array] an array of fuzzy entries
+    def fuzzy
+      find_all do |entry|
+        entry.fuzzy?
+      end
+    end
+
+    # Finds all entries that are untranslated
+    # 
+    # @return [Array] an array of untranslated entries
+    def untranslated
+      find_all do |entry|
+        entry.untranslated?
+      end
+    end
+
+    # Finds all entries that are translated
+    # 
+    # @return [Array] an array of translated entries
+    def translated
+      find_all do |entry|
+        entry.translated?
+      end
+    end
+
+    # Shows statistics and status of the provided file in percentage.
+    # 
+    # @return [Hash] a hash of translated, untranslated and fuzzy percentages
+    def stats
+      untranslated_size = untranslated.size
+      translated_size = translated.size
+      fuzzy_size = fuzzy.size
+
+      {
+        translated: percentage(translated_size),
+        untranslated: percentage(untranslated_size),
+        fuzzy: percentage(fuzzy_size)
+      }
+    end
+
+    def each
+      @entries.each do |entry|
+        yield entry
+      end
+    end
+
+    def inspect
+      "#{self.class.name}, Translated: #{stats[:translated]}% Untranslated: #{stats[:untranslated]}% Fuzzy: #{stats[:fuzzy]}%"
+    end
+
+  private
+    # calculates percentages based on total number of entries
+    # 
+    # @param [Integer] number of entries
+    # @return [Float] percentage of the provided entries
+    def percentage(size)
+      total = @entries.size
+      ((size.to_f / total) * 100).round(1)
+    end
+  end
+end
---
+++ spec/poparser/po_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe PoParser::Po do
+  let (:entry) do 
+    {
+      translator_comment: 'comment',
+      refrence: 'refrence comment',
+      msgid: 'untranslated',
+      msgstr: 'translated string'
+    }
+  end
+  
+  before(:each) do
+    @po = PoParser::Po.new
+  end
+
+  it 'should be able to add an entry to Po' do
+    # << is an alias for Po#add_entry
+    expect(@po << entry).to be_a_kind_of PoParser::Entry
+  end
+
+  it 'should be able to add multiple entries' do
+    entries = [entry, entry.dup]
+    expect(@po << entries).to be_a_kind_of Array
+  end
+
+  it 'returns all fuzzy entries' do
+    entry2, entry3 = entry.dup, entry.dup
+    [entry2, entry3].each { |en| en[:flag] = 'fuzzy' }
+    @po << [entry, entry2, entry3]
+    expect(@po.fuzzy.size).to eq 2
+  end
+
+  it 'returns all untraslated strings' do
+    entry2, entry3 = entry.dup, entry.dup
+    [entry2, entry3].each { |en| en[:msgstr] = '' }
+    @po << [entry, entry2, entry3]
+    expect(@po.untranslated.size).to eq 2
+  end
+
+  it 'shows stats' do
+    entry2, entry3 = entry.dup, entry.dup
+    [entry2, entry3].each { |en| en[:msgstr] = '' }
+    @po << [entry, entry2, entry3]
+    ap @po.stats
+  end
+end
