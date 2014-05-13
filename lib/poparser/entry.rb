dddedbf4d7fe2dae05107160799d8f24cf10bfba
Arash Mousavi <mousavi.arash@gmail.com>
2014-05-13 22:29:12 +0430

    add docs to Entry class

--- lib/poparser/entry.rb
+++ lib/poparser/entry.rb
@@ -13,33 +13,48 @@ module PoParser
     
     alias_method :translate, :msgstr=
 
+    # Checks if the entry is untraslated
+    # 
+    # @return [Boolean]
     def untranslated?
       @msgstr.nil? || @msgstr == ''
     end
     alias_method :incomplete? , :untranslated?
 
+    # Checks if the entry is translated
+    # 
+    # @return [Boolean]
     def translated?
       not untranslated?
     end
     alias_method :complete? , :translated?
 
+    # Checks if the entry is plural
+    # 
+    # @return [Boolean]
     def plural?
       @msgid_plural != nil
     end
 
+    # Checks if the entry is fuzzy
+    # 
+    # @return [Boolean]
     def fuzzy?
       @flag == 'fuzzy'
     end
 
+    # Flag the entry as Fuzzy
     def flag_as_fuzzy
       @flag = 'fuzzy'
     end
 
+    # Set flag to a custome string
     def flag_as(flag)
       raise ArgumentError if flag.class != String
       @flag = flag
     end
 
+    # Convert entry to a hash key value
     def to_h
       hash = {}
       LABELS.each do |label|
