74bffec8f7f20ffe10f083eafc27b3437a51ca2e
Arash Mousavi <mousavi.arash@gmail.com>
2014-05-14 00:23:43 +0430

    fix tokenizer test

--- spec/poparser/tokenizer_spec.rb
+++ spec/poparser/tokenizer_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper.rb"
 
 describe PoParser::Tokenizer do
-  let(:token) { PoParser::Tokenizer.new }
-  let(:po_file) { Pathname.new('spec/poparser/fixtures/tokenizer.po').realpath }
+  let(:token)  { PoParser::Tokenizer.new }
+  let(:po_file){ Pathname.new('spec/poparser/fixtures/tokenizer.po').realpath }
   let(:result) { [{:refrence=>"templates:105", :msgid=>"Afrikaans", :msgstr=>"آفریقایی"}, {:flag=>"fuzzy", :msgid=>"Afrikaans"}] }
 
   it 'should be able to extracts entries' do
     expect(
       token.extract_entries(po_file).to_h
-    ).to eq result
+    ).to eq(result)
   end
 end
