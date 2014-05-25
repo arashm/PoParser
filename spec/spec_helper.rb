b770ae1a8c026ef94f9db036be1e3ea6e9ac4b6a
Arash Mousavi <mousavi.arash@gmail.com>
2014-05-25 21:10:38 +0430

    configure coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'poparser'
 require 'parslet/rig/rspec'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
