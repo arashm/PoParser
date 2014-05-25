2856fc4820c9736e7ed817446584a1a988ccc32b
Arash Mousavi <mousavi.arash@gmail.com>
2014-05-25 22:38:50 +0430

    remove version_spec file

--- spec/poparser/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-# Just as test to ensure that test suit is working correctly
-describe 'Version' do
-  it 'shows the version correctly' do
-    expect(PoParser::VERSION).to eq('0.0.1')
-  end
-end
