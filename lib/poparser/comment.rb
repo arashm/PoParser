f496656bb6e96cb4a7e40b7882fcba80c3187616
Dennis Herr <dennis.herr@experteer.com>
2017-01-11 18:33:46 +0100

    fixed additional empty line on previous to_s(true)

--- lib/poparser/comment.rb
+++ lib/poparser/comment.rb
@@ -11,6 +11,7 @@ module PoParser
       return to_str unless with_label
       if @value.is_a? Array
         if @type.to_s =~ /^previous_/ # these behave more like messages
+          remove_empty_line
           string = ["#{COMMENTS_LABELS[@type]} \"\"\n"]
           @value.each do |str|
             string << "#| \"#{str}\"\n".gsub(/[\p{Blank}]+$/, '')
@@ -38,6 +39,13 @@ module PoParser
 
     def inspect
       @value
+    end
+
+  private
+    def remove_empty_line
+      if @value.is_a? Array
+        @value.shift if @value.first == ''
+      end
     end
   end
 end
